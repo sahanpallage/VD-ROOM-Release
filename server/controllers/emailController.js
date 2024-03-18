9443d2872574b82c611bed3002743bb7f07e3352
Pallage P.D.R.S <it21311604@my.sliit.lk>
2024-03-18 18:11:14 +0530

    f

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -2,28 +2,25 @@ import nodemailer from "nodemailer";
 import asyncHandler from "express-async-handler";
 
 export const sendEmail = asyncHandler(async (data, req, res) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
     port: 587,
     secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
-      user: "maddison53@ethereal.email",
-      pass: "jn7jnAPss4f63QBp6D",
+      user: process.env.MAIL_ID,
+      pass: process.env.MP,
     },
   });
 
-  // async..await is not allowed in global scope, must use a wrapper
-  async function main() {
-    // send mail with defined transport object
-    const info = await transporter.sendMail({
-      from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
-      to: "bar@example.com, baz@example.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Maddison Foo Koch 👻" <foo@gmail.com>', // sender address
+    to: data.to, // list of receivers
+    subject: data.subject, // Subject line
+    text: data.text, // plain text body
+    html: data.htm, // html body
+  });
 
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-  }
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
 });
