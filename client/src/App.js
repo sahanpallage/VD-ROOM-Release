9814d64726ad7f6f20f84f0ca15fc93a70ff77eb
SudaraWishvajith <it21221378@my.sliit.lk>
2024-03-25 16:23:33 +0530

    add basic scene

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
 import logo from "./logo.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import DressingRoom from "./pages/DressingRoom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <BrowserRouter> 
+    <Routes>
+    {/* <Route index element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/about" element={<About />} /> */}
+    <Route path="/droom" element={<DressingRoom />} />
+    </Routes>
+    </BrowserRouter> 
     </div>
   );
 }
