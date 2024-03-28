9f3cd1ddb09cf8aecaa0893de84b8fd13dcc90fa
Pallage P.D.R.S <it21311604@my.sliit.lk>
2024-03-28 15:43:26 +0530

    Created product card

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import MainLayout from "./components/MainLayout";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+import ForgotPassword from "./pages/ForgotPassword";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -14,6 +19,12 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/admin" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
 
 const ProductCard = () => {
   return (
     <div className="col-3">
-      <div className="product-card position-relative">
+      <Link className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+          <Link>
+            <img src="images/heart.svg" alt="wishlist" />
+          </Link>
+        </div>
         <div className="product-image">
           <img className="img-fluid" src="images/watch.png" alt="watch" />
+          <img className="img-fluid" src="images/watch-1.jpeg" alt="watch" />
         </div>
         <div className="product-details">
           <h6 className="brand">Apple</h6>
@@ -22,7 +29,23 @@ const ProductCard = () => {
           />
           <p className="price">LKR 180,000</p>
         </div>
-      </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src="images/cross-arrows.svg" alt="compare" />
+            </Link>
+            <Link>
+              <img src="images/add-cart.svg" alt="addToCart" />
+            </Link>
+            <Link>
+              <img src="images/view.svg" alt="view" />
+            </Link>
+            <Link>
+              <img src="images/vr.svg" alt="3dModel" />
+            </Link>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
