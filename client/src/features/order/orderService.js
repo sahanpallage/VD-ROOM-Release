e776ae66d53e09e9558d740218a1d9764714eb7e
Sahan Pallage <sahanpallage19@gmail.com>
2024-03-30 14:48:37 +0530

    Fixed token error

--- client/src/features/order/orderService.js
+++ client/src/features/order/orderService.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url.js";
 
-const getTokenFromLocalStorage = localStorage.getItem("user")
+const userFromLocalStorage = localStorage.getItem("user")
   ? JSON.parse(localStorage.getItem("user"))
   : null;
+
+const token = userFromLocalStorage ? userFromLocalStorage.token : null;
+
 const config = {
   headers: {
-    Authorization: `Bearer ${getTokenFromLocalStorage.token}`,
+    Authorization: `Bearer ${token}`,
     Accept: "application/json",
   },
 };
+
 const getOrders = async () => {
   const response = await axios.get(`${base_url}auth/get-allOrders`, config);
   return response.data;
--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -2,9 +2,11 @@ const getTokenFromLocalStorage = localStorage.getItem("user")
   ? JSON.parse(localStorage.getItem("user"))
   : null;
 
+const token = getTokenFromLocalStorage ? getTokenFromLocalStorage.token : null;
+
 export const config = {
   headers: {
-    Authorization: `Bearer ${getTokenFromLocalStorage.token}`,
+    Authorization: `Bearer ${token}`,
     Accept: "application/json",
   },
   // specifies the number of milliseconds before the request times out
