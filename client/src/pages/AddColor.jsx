5197b971910af5877175fc6e53b8df8ee4cf36a5
sahanpallage <sahanpallage19@gmail.com>
2024-04-30 22:23:34 +0530

    Created add color

--- client/src/pages/AddColor.jsx
+++ client/src/pages/AddColor.jsx
@@ -1,13 +1,54 @@
 import React from "react";
 import CustomInput from "../components/CustomInput";
+import customerToast from "../components/common/CustomToast";
+import { useDispatch } from "react-redux";
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import { createColor } from "../features/color/colorSlice";
+
+let Schema = Yup.object({
+  title: Yup.string().required("Color Name is required"),
+});
 
 const AddColor = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validationSchema: Schema,
+    onSubmit: (values) => {
+      if (Object.keys(formik.errors).length === 0) {
+        dispatch(createColor(values));
+        customerToast("Color Added Successfully", "success");
+        formik.resetForm();
+        setTimeout(() => {
+          navigate("/admin/color-list");
+        }, 3000);
+      } else {
+        customerToast("Color Not Added", "error");
+      }
+    },
+  });
   return (
     <div>
       <h3 className="mb-4 title">Add Color</h3>
       <div>
-        <form action="">
-          <CustomInput type="text" label="Enter Color" />
+        <form action="" onSubmit={formik.handleSubmit}>
+          <CustomInput
+            type="text"
+            name="title"
+            label="Enter A Color"
+            onCh={formik.handleChange("title")}
+            onBl={formik.handleBlur("title")}
+            val={formik.values.title}
+            id="color"
+          />
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
+          </div>
           <button
             className="btn btn-success border-0 rounded-3 my-5"
             type="submit"
